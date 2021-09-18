fde097b647a0b36e6227999b12acf71e5c0a687f
Diego <diegochoqueramirez@gmail.com>
2021-09-17 23:21:02 -0400

    add relations

--- src/main/java/com/sales/market/model/ItemInventoryEntry.java
+++ src/main/java/com/sales/market/model/ItemInventoryEntry.java
@@ -4,28 +4,47 @@
 
 package com.sales.market.model;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import java.math.BigDecimal;
 
-//@Entity
+@Entity
 public class ItemInventoryEntry extends ModelBase/*<InventoryDto>*/ {
-
+    @OneToOne
     private ItemInventory itemInventory;
     private MovementType movementType;
-    private BigDecimal quantity; // represent sale or buy instances quantity
-    private String itemInstanceSkus; //represents a list of the sku of the involved item instances
+    private BigDecimal quantity;
+    private String itemInstanceSkus;
 
-    /*
-    Take into account sku cannot be duplicated
-    In the service make possible:
-       register buy item instances -> Si no existe el producto crearlo, registrar instancias,
-                                        crear y actualizar el ItemInventory correspondiente con sus totalizados
-                                        Generar los ItemInventoryEntry para reflejar la operacion de entrada o salida
-                                         de almacen
+    public ItemInventory getItemInventory() {
+        return itemInventory;
+    }
 
-       vender un producto
-       desechar un producto similar a una venta pero a costo 0. Debe reflejar el totalizado correctamente de
-       ItemInventory
+    public void setItemInventory(ItemInventory itemInventory) {
+        this.itemInventory = itemInventory;
+    }
 
-       Debe haber tests unitarios que muestren escenarios para estas operaciones en casos de exito y de error.
-    */
+    public MovementType getMovementType() {
+        return movementType;
+    }
+
+    public void setMovementType(MovementType movementType) {
+        this.movementType = movementType;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getItemInstanceSkus() {
+        return itemInstanceSkus;
+    }
+
+    public void setItemInstanceSkus(String itemInstanceSkus) {
+        this.itemInstanceSkus = itemInstanceSkus;
+    }
 }
